7acdbcd114131d157f33f128cfdb2247316406dc
Louis-Elie Maillard <louiseliemaillard@gmail.com>
2018-04-26 17:52:58 +0200

    redirection vers ID OK

--- Pagebackend/app/controllers/users_controller.rb
+++ Pagebackend/app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
-		user = User.new
-	 	user.username = params[:username]
-	  	user.bio = params[:bio]
-	 	user.save
-	 	redirect_to userpage_path(@user)
+		@user = User.new
+	 	@user.username = params[:username]
+	  	@user.bio = params[:bio]
+	  
+	 	@user.save
+	 	redirect_to user_path(@user)
 	end
 
 	def show
-    @user = User.find_by_name(params[:username])
+    @user = User.find(params[:id])
  	end
 
 end
--- Pagebackend/config/routes.rb
+++ Pagebackend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   get '/', to: 'static_pages#home' 
-  get 'new', to: 'users#new', as: 'inscription'
-  post 'new', to: 'users#create'
-  get '/users/:user', to: 'users#show', as:'userpage'
+  get '/new', to: 'users#new', as: 'inscription'
+  post '/new', to: 'users#create'
+  get '/users/:id', to: 'users#show', as:'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
