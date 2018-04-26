d93f4b8607b14e7a471e699c1fbadc0f64f54266
Louis-Elie Maillard <louiseliemaillard@gmail.com>
2018-04-26 15:15:46 +0200

    premier user créé

--- Pagebackend/app/controllers/static_pages_controller.rb
+++ Pagebackend/app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
   
   def home
+  	puts "Hello depuis le serveur"
+	user = User.new
+	user.username = params[:username]
+	user.bio = params[:bio]
+	user.save
   end
 
 end
--- Pagebackend/app/controllers/users_controller.rb
+++ Pagebackend/app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 	
 	def new_post
+		puts "Hello depuis le serveur"
+		puts params
+		puts "c'est fini"
 	end
 
 end
